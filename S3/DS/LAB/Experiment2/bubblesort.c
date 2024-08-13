52af689542b947aa4e03a218dc71ab558476c151
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2024-08-13 12:24:44 +0530

    Update bubblesort.c

--- S3/DS/LAB/Experiment2/bubblesort.c
+++ S3/DS/LAB/Experiment2/bubblesort.c
@@ -1,1 +1,33 @@
-
+/*Pavithra Deepu E
+S3 CSAI
+Roll No:47
+Program to perform bubble sort.*/
+#include <stdio.h>
+void main()
+{
+	int a[10],x,i,j,temp,n;
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	printf("Enter the elements:  ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+			temp = a[j];
+			a[j] = a[j+1];
+			a[j+1]=temp;
+			}
+		}
+	}
+	printf("The sorted array is:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t\n",a[i]);
+	}
+}
