8291bff2cecbc54494e3f64330c711ef693d7321
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2024-10-14 10:55:22 +0530

    Update garbage.java

--- S3/OOPS/LAB/Cycle3/GarbageCollection/garbage.java
+++ S3/OOPS/LAB/Cycle3/GarbageCollection/garbage.java
@@ -1,3 +1,23 @@
 /*Pavithra Deepu E
 S3 CSEAI
 47*/
+import java.util.*;
+class test
+{
+	protected void finalize()
+	{
+		System.out.println("Object is garbage collected.");
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner S = new Scanner(System.in);
+		test S1 = new test();
+		test S2 = new test();
+
+		S1 = null;
+		S2 = null;
+		S.close();
+		System.gc();		
+	}
+}
