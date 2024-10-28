706e8131ee141a23674ff993c9ed75fb0bbb1bd8
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2024-10-28 10:02:48 +0530

    Create Infixtopostfix.c

---
+++ S3/DS/LAB/Experiment 7/Infixtopostfix.c
@@ -0,0 +1,64 @@
+/*Pavithra Deepu E
+CSE AI 47
+C program to convert Infix to Postfix*/
+
+#include <stdio.h>
+
+char stack[100];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char c) {
+    if (c == '+' || c == '-') return 1;
+    if (c == '*' || c == '/') return 2;
+    return 0;
+}
+
+int isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int isOperand(char c) {
+    return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+void infixToPostfix(char *infix, char *postfix) {
+    int i, j = 0;
+    for (i = 0; infix[i]; i++) {
+        if (isOperand(infix[i])) {
+            postfix[j++] = infix[i];
+        } else if (infix[i] == '(') {
+            push(infix[i]);
+        } else if (infix[i] == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            pop();
+        } else if (isOperator(infix[i])) {
+            while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+    }
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[100], postfix[100];
+    printf("Enter an infix expression: ");
+    scanf("%s", infix);
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
+    return 0;
+}
