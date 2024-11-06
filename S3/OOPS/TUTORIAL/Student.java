ce701d664a36497ff65a5f23dc19157d2983f1a8
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2024-11-06 10:34:45 +0530

    Add files via upload

---
+++ S3/OOPS/TUTORIAL/Student.java
@@ -0,0 +1,26 @@
+/*Pavithra Deepu E
+S3 CSEAI 47
+Program too create user defined package
+TUTORIAL*/
+
+package student; 
+
+public class Student 
+{
+
+    String name;
+    int age;
+
+    public Student(String name, int age) 
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayDetails() 
+    {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Age: " + age);
+    }
+}
+
