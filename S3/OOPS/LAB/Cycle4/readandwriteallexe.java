af3ed5b73c42b50a810caca2b4b31e32bfaeb9e0
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2024-11-06 21:08:40 +0530

    Create readandwriteallexe.java

---
+++ S3/OOPS/LAB/Cycle4/readandwriteallexe.java
@@ -0,0 +1,29 @@
+/*Pavithra Deepu E
+S3 CSE AI 47*/
+Import java.io.FileReader;
+Import java.io.FileWriter;
+Import java.io.IOException;
+Class file {
+ Public static void main(String[] args)
+ {
+ Try {
+ FileReader fr = new FileReader(“gfgInput.txt”);
+ FileWriter fw = new FileWriter(“gfgOutput.txt”);
+ String str = “”;
+ Int I;
+ While ((I = fr.read()) != -1) {
+ Str += (char)I;
+ }
+ System.out.println(str);
+ Fw.write(str);
+ Fr.close();
+ Fw.close();
+ System.out.println(
+ “File reading and writing both done”);
+ }
+ Catch (IOException e) {
+ System.out.println(
+ “There are some IOException”);
+ }
+ }
+}
