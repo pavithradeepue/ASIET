5fed0920800fb85a9128a29c98d369af2a3d0569
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2024-11-06 21:09:16 +0530

    Update readandwriteallexe.java

--- S3/OOPS/LAB/Cycle4/readandwriteallexe.java
+++ S3/OOPS/LAB/Cycle4/readandwriteallexe.java
@@ -1,5 +1,6 @@
 /*Pavithra Deepu E
-S3 CSE AI 47*/
+S3 CSE AI 47
+Java program to read and write to a file by handling all file exceptions.*/
 Import java.io.FileReader;
 Import java.io.FileWriter;
 Import java.io.IOException;
