924d53f3e68f4d9efc51997cb72d8cbaf0bfb54a
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-01-27 12:25:06 +0530

    Update sqlcammands.sql

--- S4/DBMS/sqlcammands.sql
+++ S4/DBMS/sqlcammands.sql
@@ -25,3 +25,12 @@ select year,winner,country from nobel_win1 where subject='CHEM' AND year>=1965 A
 select * from nobel_win1 where (subject='Eng' AND year=1990) UNION (select * from nobel_win1 where (subject='Phy' AND year=2022));
 select * from nobel_win1 where year = 1990 and subject not in('Phy','Chem');
 select * from nobel_win1 where subject NOT LIKE 'P%' order by year DESC; #or  asc
+
+
+create table products1(p_id number(1),p_price number(4),p_name char(5));
+insert into products1 values(&p_id,&p_price,'&p_name');
+select min(p_price) AS Minimum_price from products1;
+select max(p_price) AS Maximum_price from products1;
+select count(p_id) AS Total_number_of_products from products1;
+select sum(p_price) AS Total_price from products1;
+select avg(p_price) AS Average_price from products1;
