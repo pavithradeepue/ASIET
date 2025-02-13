bd91a0b9a15422acf3dcec7ec40b83b754db8c1e
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-02-13 11:33:17 +0530

    Update manipulativefn.sql

--- S4/DBMS/manipulativefn.sql
+++ S4/DBMS/manipulativefn.sql
@@ -89,7 +89,22 @@ create table sailors(sid int primary key,sname varchar(10),rating int,age int ch
 create table boat(bid int primary key,bname char(10),color char(10));
 create table reserves(sid int,bid int, day date,foreign key(sid) references sailors(sid),foreign key(bid) references boat(bid));
 
+insert into boat values(3,'AAA','red');
+insert into boat values(5,'Rose','yellow');
+insert into boat values(2,'Jacker','black');
+insert into boat values(7,'Ship','green');
+insert into boat values(8,'Junk','white');
 
+	insert into reserves values(5,5,'12-Feb-2025');
+select s.sname from sailors s,boat b,reserves r where s.sid = r.sid and b.bid = r.bid and(b.color = 'red' or b.color='yellow');
+
+select s.sid from sailors s where s.rating = 5 UNION select r.sid from reserves r where r.bid = 5;
+SQL> @dbms.sql
+
+       SID
+----------
+	 1
+	 5
 
 
 
