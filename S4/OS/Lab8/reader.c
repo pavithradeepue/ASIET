cf579f5a7a58544de0a00179056d270611297d74
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-03-06 11:27:52 +0530

    Create reader.c

---
+++ S4/OS/Lab8/reader.c
@@ -0,0 +1,23 @@
+/*Pavithra Deepu E
+S4 CSE AI 47*/
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+	int id;
+	void *sm;
+	char buf[100];
+	id = shmget((key_t)1222,1024,0666 | IPC_CREAT);
+	printf("Key of shared memory is %d\n",id);
+	sm = shmat(id,NULL,0);
+	printf("Process attached at %p\n",sm);
+	printf("Data read from memory: \n%s\n",(char *)sm);
+	strcpy(buf,sm);
+	int a = buf[0] - '0';
+	int b = buf[0] - '0';
+	printf("The sum is %d\n",a+b);
+}
