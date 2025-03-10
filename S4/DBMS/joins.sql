d96df5a3d7119696efeb1d143974121f2d1ed8fa
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-03-10 11:54:40 +0530

    Update joins.sql

--- S4/DBMS/joins.sql
+++ S4/DBMS/joins.sql
@@ -12,13 +12,51 @@ CREATE TABLE employees(emp_id INT PRIMARY KEY,name VARCHAR(100),dept_id INT,fore
 |     105 | Nima      |
 +---------+-----------+
 5 rows in set (0.00 sec)
-desc employees;
-+---------+--------------+------+-----+---------+-------+
-| Field   | Type         | Null | Key | Default | Extra |
-+---------+--------------+------+-----+---------+-------+
-| emp_id  | int          | NO   | PRI | NULL    |       |
-| name    | varchar(100) | YES  |     | NULL    |       |
-| dept_id | int          | YES  | MUL | NULL    |       |
-+---------+--------------+------+-----+---------+-------+
-3 rows in set (0.01 sec)
+select * from employees;
++--------+----------+---------+
+| emp_id | name     | dept_id |
++--------+----------+---------+
+|      1 | Nima     |     105 |
+|      2 | Pavithra |     101 |
+|      3 | Prithvi  |     102 |
+|      4 | Roshan   |     103 |
+|      5 | Sidharth |     104 |
++--------+----------+---------+
+5 rows in set (0.00 sec)
+//COMPANY ACCEPTED FORMAT\\
+ SELECT e.name,d.dept_name FROM employees e INNER JOIN departments d ON e.dept_id = d.dept_id;
++----------+-----------+
+| name     | dept_name |
++----------+-----------+
+| Pavithra | Pavithra  |
+| Prithvi  | Prithvi   |
+| Roshan   | Roshan    |
+| Sidharth | Sidharth  |
+| Nima     | Nima      |
++----------+-----------+
+5 rows in set (0.00 sec)
+//OTHER FORMATS\\
+SELECT name,dept_name FROM employees e INNER JOIN departments d ON e.dept_id = d.dept_id;
+Select employees.name,departments.dept_name FROM employees INNER JOIN departments ON employees.dept_id = departments.dept_id;
+select name,dept_name FROM employees INNER JOIN departments ON employees.dept_id = departments.dept_id;
+SELECT name,dept_name FROM employees e INNER JOIN departments d ON e.dept_id = d.dept_id;
+
+///////In mysql full join wont work so we use left and right and union cheyyum but in oracle it'll work\\\\\\\\\
+SELECT employees.name,departments.dept_name from employees LEFT JOIN departments ON employees.dept_id = departments.dept_id UNION SELECT employees.name,departments.dept_name FROM employees RIGHT JOIN departments ON employees.dept_id = departments.dept_id;
+
+//left outer and right outer\\
+SELECT e.name,d.dept_name FROM employees e LEFT OUTER JOIN departments d ON e.dept_id = d.dept_id;
+SELECT e.name,d.dept_name FROM employees e RIGHT OUTER JOIN departments d ON e.dept_id = d.dept_id;
+
+ 
+
+
+
+
+
+
+
+
+
+
 
