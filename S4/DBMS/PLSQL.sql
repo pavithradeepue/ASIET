a250c33107a801689e9e2f75d51d2e73db48f9c7
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-03-24 11:27:10 +0530

    Create PLSQL.sql

---
+++ S4/DBMS/PLSQL.sql
@@ -0,0 +1,26 @@
+DECLARE 
+a number;
+b number;
+c number;
+PROCEDURE finndMin(x IN number,y IN number,z OUT number) IS
+BEGIN 
+ if x < y THEN
+  z:= x;
+ ELSE
+  z:= y;
+ END IF;
+END;
+
+BEGIN
+a:=23;
+b:=45;
+findMin(a,b,c);
+dbms_output.put_line('Minimum of (23,45): '||c);
+END;
+/
+
+/*OUTPUT*/
+SQL> @PLsql.sql    
+Minimum of (23,45): 23
+
+PL/SQL procedure successfully completed.
