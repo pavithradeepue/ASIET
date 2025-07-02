b0c8e9ff20f0eabea270450dbabea463a35a0aa3
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-07-02 09:24:17 +0530

    Update 30.py

--- S5/Python/30.py
+++ S5/Python/30.py
@@ -1,3 +1,16 @@
 #average
 #convert temp def
 #recursive sum of nat numbers
+
+#Program to count vowels and consonants
+s = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+v,c = 0,0
+for ch in s:
+	if ch.isalpha():
+		if ch in vowels:
+			v += 1
+		else:
+			c += 1
+print("Vowels: ",v,"Consonants: ",c)
+print("Number of letters in the string is: ",len(s))
