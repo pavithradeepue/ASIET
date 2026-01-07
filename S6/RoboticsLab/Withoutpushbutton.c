b59595a1902d1e8d67a36a811aa607255a759c8a
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2026-01-07 09:22:05 +0530

    Add Withoutpushbutton.c for LED blinking control

---
+++ S6/RoboticsLab/Withoutpushbutton.c
@@ -0,0 +1,13 @@
+int led1 = 2;
+void setup()
+{
+  pinMode(led1, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(led1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
